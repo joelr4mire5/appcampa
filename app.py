144d4586bb474ac9a8408833c74ac8ef8805b86e
science <joel.r4mire5@outlook.com>
2024-01-10 21:45:35 -0600

    final version

--- app.py
+++ app.py
@@ -138,7 +138,6 @@ dashboard_data.loc[condition, 'puntaje'] *= 6
 dashboard_data['total_puntaje']=dashboard_data["puntos"]*dashboard_data["puntaje"]
 
 
-st.write(dashboard_data)
 
 dashboard_data.rename(columns={"puntaje":"cantidad_versiculos"},inplace=True)
 
