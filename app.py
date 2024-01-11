1d76ba9c887ad8b83b2ef614330d78120ac255a2
science <joel.r4mire5@outlook.com>
2024-01-10 21:24:14 -0600

    test3

--- app.py
+++ app.py
@@ -133,7 +133,7 @@ values=[10,150,230]
 dashboard_data['puntos'] = np.select(conditions, values)
 
 dashboard_data['total_puntaje']=dashboard_data["puntos"]*dashboard_data["puntaje"]
-dashboard_data.loc[dashboard_data['nombre completo'].isin(['Deiand Martinez', 'Sofia Dinarte']), 'puntajes totales'] = 6
+dashboard_data.loc[dashboard_data['nombrecompleto'].isin(['Deiand Martinez', 'Sofia Dinarte']), 'puntajes totales'] = 6
 
 st.write(dashboard_data)
 
