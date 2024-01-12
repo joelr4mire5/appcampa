e0eded6d5803507c7f8ed294fdd4f38c66512686
science <joel.r4mire5@outlook.com>
2024-01-11 21:07:24 -0600

    Dashboard nuevo test1

--- app.py
+++ app.py
@@ -157,9 +157,10 @@ resumen_equipo_cantidad_versiculos=resumen_equipo_cantidad_versiculos.reset_inde
 resumenparticipantes=dashboard_data.groupby(by=['nombrecompleto','equipo','categoria'])['cantidad_versiculos'].sum()
 
 
-# dashboard_data_por_participante=dashboard_data[dashboard_data==nombre_selecccionado]
-# st.title("Resumen por campista")
-st.dataframe(dashboard_data)
+dashboard_data_por_participante=dashboard_data
+dashboard_data_por_participante=dashboard_data_por_participante[dashboard_data_por_participante==nombre_selecccionado]
+st.title("Resumen por campista")
+st.dataframe(dashboard_data_por_participante)
 
 st.title("Total de puntaje por equipos")
 st.bar_chart(resumen_equipo_total_puntaje, x="equipo", y="total_puntaje", color='equipo')
